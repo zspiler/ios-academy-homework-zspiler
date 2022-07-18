8ea4b1352d0db48a47980e5768a3f9978f72f4bc
Zan Spiler <zanspiler1@gmail.com>
2022-07-18 16:09:25 +0200

    styled login screen

---
+++ TV Shows/TV Shows/Login/Colors.swift
@@ -0,0 +1,17 @@
+//
+//  Colors.swift
+//  TV Shows
+//
+//  Created by Infinum on 16.07.2022..
+//
+
+import UIKit
+
+struct Colors {
+    static let disabledLoginButtonBackground = UIColor.white.withAlphaComponent(0.3)
+    static let enabledLoginButtonBackground = UIColor.white
+    static let disabledLoginButtonTitle = UIColor.white.withAlphaComponent(0.4)
+    static let enabledLoginButtonTitle = UIColor(red: 0.32, green: 0.21, blue: 0.55, alpha: 1.00)
+    static let disabledRegisterButtonTitle = UIColor.white.withAlphaComponent(0.4)
+    static let enabledRegisterButtonTitle = UIColor.white
+}
--- TV Shows/TV Shows/Login/LoginViewController.swift
+++ TV Shows/TV Shows/Login/LoginViewController.swift
@@ -10,13 +10,19 @@ import UIKit
 final class LoginViewController: UIViewController {
     
     // MARK: - Outlets
-
-    @IBOutlet weak var textLabel: UILabel!
-
+    
+    @IBOutlet weak var emailInput: UITextField!
+    @IBOutlet weak var passwordInput: UITextField!
+    @IBOutlet weak var rememberMeCheckbox: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var showPasswordButton: UIButton!
+    
     // MARK: - Properties
     
-    private var numOfTaps = 0
-
+    var emailText = ""
+    var passwordText = ""
+    
     // MARK: - Lifecycle methods
     
     override func viewDidLoad() {
@@ -26,16 +32,76 @@ final class LoginViewController: UIViewController {
     
     // MARK: - Actions
     
-    @IBAction func tapButton() {
-        numOfTaps += 1
-        textLabel.text = String(numOfTaps)
+    @IBAction func tapShowPasswordButton() {
+        showPasswordButton.isSelected = !showPasswordButton.isSelected
+        passwordInput.isSecureTextEntry = !showPasswordButton.isSelected
+    }
+    
+    @IBAction func tapRememberMeCheckbox() {
+        rememberMeCheckbox.isSelected = !rememberMeCheckbox.isSelected
+    }
+    
+    @IBAction func changeEmailInputText(_ sender: UITextField) {
+        emailText = sender.text ?? ""
+        
+        updateLoginRegisterButtons()
+    }
+    
+    @IBAction func changePasswordInputText(_ sender: UITextField) {
+        passwordText = sender.text ?? ""
+        showPasswordButton.isHidden = passwordText.count == 0
+        
+        updateLoginRegisterButtons()
     }
     
     // MARK: - Helpers
     
     func setUpUI() {
-        textLabel.text = String(numOfTaps)
+        loginButton.layer.cornerRadius = 24
+        
+        rememberMeCheckbox.setImage(UIImage(named: "ic-checkbox-unselected"), for: UIControl.State.normal)
+        rememberMeCheckbox.setImage(UIImage(named: "ic-checkbox-selected"), for: UIControl.State.selected)
+        
+        showPasswordButton.setImage(UIImage(named: "ic-visible"), for: UIControl.State.normal)
+        showPasswordButton.setImage(UIImage(named: "ic-invisible"), for: UIControl.State.selected)
+        
+        
+        let placeholderFont = UIFont.systemFont(ofSize: 17, weight: .light)
+        
+        emailInput.attributedPlaceholder = NSAttributedString(
+            string: "Email",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.white.withAlphaComponent(0.7),
+                         NSAttributedString.Key.font: placeholderFont
+                        ]
+        )
+        
+        passwordInput.attributedPlaceholder = NSAttributedString(
+            string: "Password",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.white.withAlphaComponent(0.7), NSAttributedString.Key.font: placeholderFont]
+        )
+        
+        
+        disableLoginRegisterButtons()
+        
     }
-
+    
+    func updateLoginRegisterButtons() {
+        if emailText.count > 0 && passwordText.count > 0 {
+            enableLoginRegisterButtons()
+        } else {
+            disableLoginRegisterButtons()
+        }
+    }
+    
+    func disableLoginRegisterButtons() {
+        loginButton.tintColor = Colors.disabledLoginButtonTitle
+        loginButton.backgroundColor = Colors.disabledLoginButtonBackground
+        registerButton.tintColor = Colors.disabledRegisterButtonTitle
+    }
+    
+    func enableLoginRegisterButtons() {
+        loginButton.tintColor = Colors.enabledLoginButtonTitle
+        loginButton.backgroundColor = Colors.enabledLoginButtonBackground
+        registerButton.tintColor = Colors.enabledRegisterButtonTitle
+    }
 }
-
